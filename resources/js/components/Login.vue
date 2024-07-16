274fe17cbbb748444708724c6efc19d4027649d5
jamezzz123 <chinwokwuj@gmail.com>
2024-07-16 21:54:32 +0100

    refactor: added Trivia Screen

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,58 +1,59 @@
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div class="max-w-md w-full space-y-8">
-        <div>
-          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to Trivia Game
-          </h2>
-        </div>
-        <form class="mt-8 space-y-6" @submit.prevent="handleLogin">
-          <div class="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label for="phone-number" class="sr-only">Phone number</label>
-              <input id="phone-number" name="phone" type="tel" required
-                     class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                     placeholder="Phone number"
-                     v-model="phoneNumber">
-            </div>
-          </div>
-  
-          <div>
-            <button type="submit"
-                    class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Sign in
-            </button>
-          </div>
-        </form>
-      </div>
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h2>
     </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  import axios from 'axios';
-  
-  export default {
-    emits: ['login-success'],
-    setup(props, { emit }) {
-      const phoneNumber = ref('');
-  
-      const handleLogin = async () => {
-        try {
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label class="block text-sm font-medium leading-6 text-gray-900">Phone
+              Number</label>
+          </div>
+          <div class="mt-2">
+            <input v-model="phoneNumber" name="phone_number" type="text"
+              class="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+
+        <div class="flex space-y-2 flex-col items-center justify-between">
+          <button
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
+          <button @click.prevent="router.push('/login')" type="submit"
+            class="flex w-full justify-center rounded-md  px-3 py-1.5 text-sm font-semibold leading-6 text-indigo-600  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+            up</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
+
+export default {
+  emits: ['login-success'],
+  setup(props, { emit }) {
+    const phoneNumber = ref('');
+
+    const handleLogin = async () => {
+      try {
         //   const response = await axios.post('/api/login', { phone_number: phoneNumber.value });
-          localStorage.setItem('token', response.data.token);
+        localStorage.setItem('token', response.data.token);
         //   axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
-          emit('login-success');
-        } catch (error) {
-          console.error('Login failed:', error);
-          // Handle login error (show message to user)
-        }
-      };
-  
-      return {
-        phoneNumber,
-        handleLogin
-      };
-    }
+        emit('login-success');
+      } catch (error) {
+        console.error('Login failed:', error);
+        // Handle login error (show message to user)
+      }
+    };
+
+    return {
+      phoneNumber,
+      handleLogin
+    };
   }
-  </script>
+}
+</script>
--- resources/js/components/Trivia.vue
+++ resources/js/components/Trivia.vue
@@ -1,5 +1,62 @@
 <template>
-    <div>Vue js {{ hello }}</div>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <template v-if="!beforeStart">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Welcome to
+                    Trivia
+                    Game</h2>
+            </div>
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div class="flex space-y-2 flex-col items-center justify-between">
+                    <button @click.prevent="beforeStart = !beforeStart"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Click
+                        to Start</button>
+                </div>
+            </div>
+        </template>
+        <!-- <template v-else>
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Who is the
+                    President of Nigeria</h2>
+            </div>
+            <div class="space-y-4 mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div class="flex items-center">
+                    <input id="option1" name="options" type="radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+                    <label for="option1" class="ml-2 block text-base font-medium text-gray-900">Option 1</label>
+                </div>
+                <div class="flex items-center">
+                    <input id="option2" name="options" type="radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+                    <label for="option2" class="ml-2 block text-base font-medium text-gray-900">Option 2</label>
+                </div>
+                <div class="flex items-center">
+                    <input id="option3" name="options" type="radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+                    <label for="option3" class="ml-2 block text-base font-medium text-gray-900">Option 3</label>
+                </div>
+            </div>
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div class="flex space-y-2 flex-col items-center justify-between">
+                    <button
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Next</button>
+                </div>
+            </div>
+        </template> -->
+        <template v-else>
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Good Job <span class="text-indigo-600">John Doe</span></h2>
+                <h2 class="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900">Score: <span class="text-indigo-600">8/10</span></h2>
+            </div>
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div class="flex space-y-2 flex-col items-center justify-between">
+                    <button
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Restart</button>
+                </div>
+            </div>
+        </template>
+
+    </div>
 </template>
 
 <script>
@@ -7,6 +64,7 @@ export default {
     name: 'Trivia',
     data() {
         return {
+            beforeStart: false,
             hello: 'helloooooo0000'
         }
     }
