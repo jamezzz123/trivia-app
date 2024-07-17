b468df26a0e084b3f589b000d3f4a251bbe016e3
jamezzz123 <chinwokwuj@gmail.com>
2024-07-17 22:10:18 +0100

    refactor: added timer to the triva quiz

--- resources/js/components/Trivia.vue
+++ resources/js/components/Trivia.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <p class="text-center text-lg font-semibold leading-6 text-gray-900 text-indigo-600 ">{{ formattedTime }}
+            </p>
+        </div>
+
         <template v-if="!beforeStart">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Welcome to
@@ -39,8 +44,8 @@
         <template v-if="beforeStart && done">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Good Job</h2>
-                <h2 class="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900">here is your score: <span
-                        class="text-indigo-600">{{ calculateScore() }}/{{ questions.length }}</span></h2>
+                <h2 class="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900">here is your
+                    score: <span class="text-indigo-600">{{ calculateScore() }}/{{ questions.length }}</span></h2>
             </div>
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <div class="flex space-y-2 flex-col items-center justify-between">
@@ -63,10 +68,26 @@ export default {
             done: false,
             questions: [],
             currentQuestionIndex: 0,
+
+            timeLeft: 30, // set your desired countdown time in seconds
+            interval: null
         }
     },
 
     methods: {
+        startCountdown() {
+            this.interval = setInterval(() => {
+                if (this.timeLeft > 0) {
+                    this.timeLeft--;
+                } else {
+                    clearInterval(this.interval);
+                    this.onTimeUp();
+                }
+            }, 1000);
+        },
+        pad(number) {
+            return number < 10 ? '0' + number : number;
+        },
         getTrivial() {
             axios.get('/api/questions')
                 .then(response => {
@@ -79,6 +100,7 @@ export default {
                         }
                     });
                     this.beforeStart = true;
+                    this.startCountdown();
                 })
         },
         nextQuestion() {
@@ -92,17 +114,33 @@ export default {
             this.beforeStart = false;
             this.done = false;
             this.currentQuestionIndex = 0;
+            this.timeLeft = 30;
         },
 
         calculateScore() {
             let result = this.questions.reduce((acc, item) => {
-                if (Number(item.selected_option) == Number(item.correct_option)) {
+                if (item.selected_option == item.correct_option) {
                     acc++;
                 }
                 return acc
             }, 0);
             return result
+        },
+        onTimeUp() {
+            // Handle what happens when the timer reaches zero
+            this.done   = true
         }
     },
+
+    computed: {
+        formattedTime() {
+            const minutes = Math.floor(this.timeLeft / 60);
+            const seconds = this.timeLeft % 60;
+            return `${this.pad(minutes)}:${this.pad(seconds)}`;
+        }
+    },
+    beforeDestroy() {
+        clearInterval(this.interval);
+    }
 }
 </script>
